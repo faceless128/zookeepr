32fb6defe483c36d82e87b8149567582503b6064
Kaijam Jeffers <faceless128@gmail.com>
2022-07-16 00:10:10 -0400

    updated server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
